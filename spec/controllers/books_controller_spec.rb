53b6fec684915d9d2f55574399706237d8ae3612
Your Name <you@example.com>
2016-03-24 22:21:31 +0200

    Unit test - Books: Fix typo

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe BooksController do
     it 'saves the release date' do
       # Logic
       release_date = Date.today
-      post :create, book: { isbn:'1234567890123', title 'Harry Potter', release_date: release_date }
+      post :create, book: { isbn:'1234567890123', title: 'Harry Potter', release_date: release_date }
       expect(Book.find_by(isbn: '1234567890123').release_date).to eq(release_date))
     end
   end
